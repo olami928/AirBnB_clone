b78d49208342968b329846d217be8bcc014868dc
olami928 <oladimejialabi45@7-add_item.pygmail.com>
2024-06-29 16:27:24 +0100

    hot fix

--- models/base_model.py
+++ models/base_model.py
@@ -25,20 +25,20 @@ class BaseModel:
     def __str__(self):
         """ This is the string representation of BaseModel."""
 
-        return (f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}")
+        return (f"{[type(self).__name__]} ({self.id}) {self.__dict__}")
 
     def save(self):
         """Updates the public instance updated_at with the current datetime."""
 
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def to_dict(self):
         """ Returns a dictionary conatining all keys/values of __dict__
         of the instance.
         """
 
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict = {**self.__dict__}
+        obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return (obj_dict)
